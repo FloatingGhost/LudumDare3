1a78e02ab7d0faa3f828b8c35e1fe5e1024a8bf5
Hannah Ward <harry.ward9001@gmail.com>
2015-08-22 12:43:45 +0100

    Raycasting for enemy vision

--- js/states/maingame.js
+++ js/states/maingame.js
@@ -10,6 +10,7 @@ MainGame.prototype = {
         this.load.tilemap('map', 'res/map/Map', null, Phaser.Tilemap.CSV);
         this.load.image('tiles', 'res/img/sprites.png');
         this.load.image("player", "res/img/player.png");
+        this.load.image("enemy", "res/img/enemy.png");
     },
 
     create: function() {
@@ -19,6 +20,8 @@ MainGame.prototype = {
         this.endX = 0;
         this.endY = 0;
         this.cooldown = 0;
+
+        this.money = 100;
 
         //SCREW THIS WITH A VENGEANCE
 
@@ -40,23 +43,25 @@ MainGame.prototype = {
         for (var y = 0; y<15; y++) {
             var toPush = [];
             for (var x = 0; x < 15; x++) {
-                toPush.push((this.map.getTile(this.layer.getTileX(x*32), this.layer.getTileY(y*32)).index==1)?0:1);
+                toPush.push((this.map.getTile(this.layer.getTileX(x*32), this.layer.getTileY(y*32)).index==1)?1:0);
             }
             this.grid.push(toPush);
         }
         console.log(this.grid);
 
         this.pf = new PF.Grid(this.grid);
-        this.path = new PF.AStarFinder();
+        this.path = new PF.BestFirstFinder();
 
         this.players = [];
 
         for (var i = 0; i<4; i++) {
-            var player = this.add.sprite(120*i, 120*i, "player");
+            var player = this.add.sprite(64*i, 64*i, "player");
             player.selected = false;
             player.alpha = 1;
             this.players.push(player);
         }
+
+        this.enemy = this.add.sprite(13*32, 0, "enemy");
 
 
     },
@@ -75,6 +80,31 @@ MainGame.prototype = {
 
 
     update: function() {
+        var cansee = [];
+
+        //Raycast from player
+        for (var i=0; i<2*Math.PI; i+= (2*Math.PI)/32) {
+            for (var j = 0; j<20; j++) {
+                try {
+                    var tile = this.map.getTile(this.layer.getTileX(this.enemy.x + 32*j*Math.cos(i)), this.layer.getTileY(this.enemy.y + 32*j*Math.sin(i)));
+                    if (tile.index == 1) {
+                        break;
+                    } else {
+                        for (var q in this.players) {
+                            var p = this.players[q];
+                            if (this.clickedOn(p, this.enemy.x + 32*j*Math.cos(i), this.enemy.y + 32*j*Math.sin(i))) {
+                               cansee.push(p);
+                                console.log("I SEE!");
+                            }
+                        }
+
+                    }
+                } catch (e) {
+                    break;
+                }
+            }
+        }
+
         if (this.input.mousePointer.isDown && this.cooldown <= 0) {
             for (i in this.players) {
                 this.cooldown = 20;
@@ -102,14 +132,14 @@ MainGame.prototype = {
                         x = this.convertToWorld(x);
                         console.log("X:", x);
                         if (x[0][0] != undefined) {
-                            var t = this.add.tween(this.players[i]).to({x: x[0], y: x[1]}, 500 * x[0].length).start();
+                            var t = this.add.tween(this.players[i]).to({x: x[0], y: x[1]}, 300 * x[0].length).start();
                         }
                         this.pf = gridBackup;
                         this.players[i].selected = false;
                         this.players[i].tween.stop();
                         this.players[i].tween = undefined;
                         this.players[i].alpha = 1;
-
+                        break;
 
                     }
                 }
@@ -126,7 +156,7 @@ MainGame.prototype = {
 
 
     clickedOn: function(spr, x, y) {
-        return x > spr.x && x < spr.x + spr.width && y > spr.y && y < spr.y + spr.height;
+        return x >= spr.x && x <= spr.x + spr.width && y >= spr.y && y <= spr.y + spr.height;
     }
 
 
